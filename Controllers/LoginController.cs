09bdb24c81605739a086796a4bea6e7f26c160fd
BrayanConde23 <soycondebrayan@gmail.com>
2024-08-22 07:12:48 -0500

    feat: Se agrega vista front del login en views

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -5,28 +5,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
 namespace Library.Controllers
 {
-    [Route("[controller]")]
     public class LoginController : Controller
     {
-        private readonly ILogger<LoginController> _logger;
-
-        public LoginController(ILogger<LoginController> logger)
-        {
-            _logger = logger;
-        }
-
+        // GET: /Login/Index
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // POST: /Login/Index
+        [HttpPost]
+        public IActionResult Index(string correo, string clave)
+        {
+            // Lógica de autenticación
+            if (correo == "Konoe@gmail.com" && clave == "123")
+            {
+                // Redirige a otra acción o vista
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                ViewBag.Error = "Credenciales incorrectas.";
+                return View();
+            }
+        }
+
+        // GET: /Login/Error
         public IActionResult Error()
         {
-            return View("Error!");
+            return View();
         }
     }
 }
